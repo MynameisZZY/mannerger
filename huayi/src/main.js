2518c760068544b549bcfc7e66f1bdea57dcac40
张子寅 <1940098426@qq.com>
2018-12-30 20:22:13 +0800

    2018-12-30

--- huayi/src/main.js
+++ huayi/src/main.js
@@ -3,6 +3,13 @@ import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
 import './assets/styles/public.css'
+import axios from 'axios'//引入axios
+import qs from 'qs'//引入qs
+
+Vue.prototype.axios=axios;
+// 把axios挂载到原型上
+Vue.prototype.qs=qs;
+// 把qs挂载到原型上
 
 Vue.config.productionTip = false
 
--- huayi/src/views/accountManagement.vue
+++ huayi/src/views/accountManagement.vue
@@ -1,110 +1,93 @@
 <template>
 
-<div id="app">
- <!-- -------------------------------------------左部导航栏-------------------------------- -->
+  <div id="app">
+    <!-- -------------------------------------------左部导航栏-------------------------------- -->
 
+    <el-container id="container">
 
- <el-container id="container">
+      <Leftmenu></Leftmenu>
+      <el-container id="mainContent">
 
-<Leftmenu></Leftmenu>
-<el-container id="mainContent">
- 
+        <!-- ---------------------------------------------头部部分-------------------------------------- -->
+        <RightTop></RightTop>
+        <!-- -----------------------------------------------main部分------------------------------------- -->
+        <!-- 卡片 -->
 
-<!-- ---------------------------------------------头部部分-------------------------------------- -->
-    <RightTop></RightTop>
-<!-- -----------------------------------------------main部分------------------------------------- -->
-<!-- 卡片 -->
-<el-card class="box-card">
-  <div slot="header" class="clearfix">
-    <span>添加管理员账号</span>
-    <el-button style="float: right; padding: 3px 0" type="text"></el-button>
+        <el-card class="box-card">
+          <div slot="header" class="clearfix">
+            <span>账号管理</span>
+
+          </div>
+          <div class="text item">
+            <!-- 表格 -->
+            <el-table :data="tableData" style="width: 100%">
+              <el-table-column label="用户名称">
+                <template slot-scope="scope">
+                  <i class="el-icon-time"></i>
+                  <span style="margin-left: 10px">{{ scope.row.date }}</span>
+                </template>
+              </el-table-column>
+              <el-table-column label="用户组" >
+                <template slot-scope="scope">
+                  <el-popover trigger="hover" placement="top">
+                    <p>姓名: {{ scope.row.name }}</p>
+                    <p>住址: {{ scope.row.address }}</p>
+                    <div slot="reference" class="name-wrapper">
+                      <el-tag size="medium">{{ scope.row.name }}</el-tag>
+                    </div>
+                  </el-popover>
+                </template>
+              </el-table-column>
+              <el-table-column label="管理">
+                <template slot-scope="scope">
+                  <el-button size="mini" @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+                  <el-button size="mini" type="danger" @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+                </template>
+              </el-table-column>
+            </el-table>
+
+          </div>
+        </el-card>
+
+        <!-------------------------------------------------------------------------------- footer ------------------- -->
+        <Footer></Footer>
+        <!--  -----------------------------------------------------------分界------------------------------------------ -->
+      </el-container>
+    </el-container>
   </div>
-  <div class="text item">
-   <!-- 表单元素 -->
-
-<el-form :model="ruleForm" :rules="rules" label-position="top" ref="ruleForm" label-width="100px" class="demo-ruleForm">
-  <el-form-item label="用户名" prop="name">
-    <el-input v-model="ruleForm.name"></el-input>
-  </el-form-item>
-
-  
-  <el-form-item label="密码" prop="name">
-    <el-input v-model="ruleForm.name"></el-input>
-  </el-form-item>
-
-  <el-form-item label="确认密码" prop="name">
-    <el-input v-model="ruleForm.name"></el-input>
-  </el-form-item>
-
-  <el-form-item label="选择用户组" prop="region">
-    <el-select v-model="ruleForm.region" placeholder="请选择活动区域">
-      <el-option label="普通会员" value="普通会员"></el-option>
-      <el-option label="超级会员" value="超级会员"></el-option>
-    </el-select>
-  </el-form-item>
-  
-  <el-form-item>
-    <el-button type="primary" @click="submitForm('ruleForm')">立即创建</el-button>
-    <el-button @click="resetForm('ruleForm')">重置</el-button>
-  </el-form-item>
-</el-form>
-
-
-  </div>
-</el-card>
-  
-
-
-<!-------------------------------------------------------------------------------- footer ------------------- -->
- <Footer></Footer>
-<!--  -----------------------------------------------------------分界------------------------------------------ -->
-  </el-container>
-</el-container>
-</div>
 </template>
 
 <script>
 //引入组件
-import Leftmenu from '../components/leftMenu'
-import RightTop from '../components/rightTop'
-import Footer from '../components/footer'
+import Leftmenu from "../components/leftMenu";
+import RightTop from "../components/rightTop";
+import Footer from "../components/footer";
 export default {
-  
   data() {
     return {
       //表格数据
-      
-       ruleForm: {
-          name: '',
-          region: '',
-        
-        },rules:{
-          name: [
-            { required: true, message: '请输入活动名称', trigger: 'blur' },
-            { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
-          ],
-          region: [
-            { required: true, message: '请选择活动区域', trigger: 'change' }
-          ],
-        }, resetForm(formName) {
-        this.$refs[formName].resetFields();
+
+        tableData: [{
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }]
       }
-    
-    };
-  },
-  components:{ //注册组件
+    },
+    methods: {
+      handleEdit(index, row) {
+        // console.log(index, row);
+      },
+      handleDelete(index, row) {
+        // console.log(index, row);
+      }
+    },
+
+  components: {
+    //注册组件
     Leftmenu,
     RightTop,
     Footer
   }
 };
 </script>
-
-<style>
-.el-card__body{
-  overflow-y:scroll;
-  height: 470px;
-}
-</style>
-
-
